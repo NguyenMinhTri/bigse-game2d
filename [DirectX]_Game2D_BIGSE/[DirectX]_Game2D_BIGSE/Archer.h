7e3647099cb119091c48358d8c539aa242c748e6
kienchochethahaha <kienchochethahaha@cadc82a3-7b50-c7c1-68c1-a78f6cd06ff6>
2011-12-07 04:46:35 +0000

    + Confli

--- [DirectX]_Game2D_BIGSE/[DirectX]_Game2D_BIGSE/Archer.h
+++ [DirectX]_Game2D_BIGSE/[DirectX]_Game2D_BIGSE/Archer.h
@@ -9,33 +9,13 @@ class Archer :
 	public Character
 {
 protected:
-	Sprite *m_Archer;
-	bool arrow;
-	bool COOLDOWN;
 public:
 	Archer(void);
 	~Archer(void);
 	
 	virtual void Init () ;
-	
-
-	/* Update Move and check Collision with Terrain */
-	
-
-	/* Update Animation */
 	virtual void Animation (float _Time) ;
 
 
-
-	/* Draw Object */
-	virtual void Draw (D3DXMATRIX _MWorld,LPD3DXSPRITE _Handler) ;
-
-	/* Release */
-	virtual void Release () ;
-
-	virtual void ActiveSkill (int _Index);
-
-	/* Set Jump */
-	void setJump ();
 };
 
